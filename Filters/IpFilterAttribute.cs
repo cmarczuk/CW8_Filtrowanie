e8376d5c625045d3fd5eeba5d516c393ef22367e
Cezary Marczuk <77850@student.pb.edu.pl>
2021-05-20 11:51:41 +0200

    Dodaj pliki projektów.

---
+++ Filters/IpFilterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CW8_Filtrowanie.Filters
+{
+    public class IpFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var result = context.Result;
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                var ip = context.HttpContext.Connection.RemoteIpAddress;
+                /*if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+                {
+                    ip = System.Net.Dns.GetHostEntry(ip).AddressList.First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+                }*/
+                if (ip.IsIPv4MappedToIPv6)
+                {
+                    ip = ip.MapToIPv4();
+                }
+                page.ViewData["ip"] = ip.MapToIPv4();
+            }
+        }
+    }
+}
